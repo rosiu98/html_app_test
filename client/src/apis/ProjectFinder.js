482fe6529c3e07f2fb01e23cc84f6ff57276dfb7
Pawel Rosiek <rosiu978@gmail.com>
2023-10-23 01:07:16 +0200

    adding check for NODE_ENV

--- client/src/apis/ProjectFinder.js
+++ client/src/apis/ProjectFinder.js
@@ -3,9 +3,11 @@ import axios from "axios";
 // NODE_ENV = 'development'
 // NODE_ENV = 'production'
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
 const baseURL = process.env.NODE_ENV === 'production' ? 
 "https://html-app-backend.vercel.app/api/v1/projects" :  
-"https://html-app-backend.vercel.app/api/v1/projects"
+"http://localhost:3001/api/v1/projects"
 
 
 export default axios.create({
