0bddc0f63dda58c599b0c00d3a610daf5ffc10c6
Pawel Rosiek <rosiu978@gmail.com>
2023-10-23 01:24:39 +0200

    Updating to vercel prod

--- client/src/apis/ProjectFinder.js
+++ client/src/apis/ProjectFinder.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // NODE_ENV = 'development'
 // NODE_ENV = 'production'
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
 
 const baseURL = process.env.NODE_ENV === 'production' ? 
 "https://html-app-backend.vercel.app/api/v1/projects" :  
