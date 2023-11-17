1ff0cfd9d33ce433f05e91006fa19c1c534e467f
Pawel Rosiek <rosiu978@gmail.com>
2023-11-17 21:48:12 +0100

    adding list functinality back and shellEnergy option

--- client/src/apis/lists.js
+++ client/src/apis/lists.js
@@ -1,86 +1,108 @@
 export const listOfContentBlocks = [
-    {
-        value: 'Preheader',
-        text: 'Preheader',
-    },
-    {
-        value: 'Logo',
-        text: 'Logo',
-    },
-    {
-        value: 'Header',
-        text: 'Header',
-    },
-    {
-        value: 'Body',
-        text: 'Body',
-    },
-    {
-        value: 'Columns',
-        text: 'Columns',
-    },
-    {
-        value: 'OneCta',
-        text: 'OneCta',
-    },
-    {
-        value: 'Rating',
-        text: 'Rating',
-    },
-    {
-        value: 'Footer',
-        text: 'Footer',
-    }
-]
-
+  {
+    value: "Preheader",
+    text: "Preheader",
+  },
+  {
+    value: "Logo",
+    text: "Logo",
+  },
+  {
+    value: "Header",
+    text: "Header",
+  },
+  {
+    value: "Body",
+    text: "Body",
+  },
+  {
+    value: "Columns",
+    text: "Columns",
+  },
+  {
+    value: "OneCta",
+    text: "OneCta",
+  },
+  {
+    value: "Rating",
+    text: "Rating",
+  },
+  {
+    value: "Footer",
+    text: "Footer",
+  },
+];
 
 export const listOfCategories = [
-    {
-        value: 'Cibc',
-        text: 'Cibc',
-        icon: <img src="https://i.imgur.com/f8llVJY.png" width='16' alt="CIBC" />
-    },
-    // {
-    //     value: 'MountainDew',
-    //     text: 'MountainDew',
-    //     icon: <img src="https://i.imgur.com/ih7vLVS.png" width='16' alt="MountainDew" />
-    // },
-    // {
-    //     value: 'TastyRewards',
-    //     text: 'TastyRewards',
-    //     icon: <img src="https://image.em.pepsico.com/lib/fe4515707564067f771671/m/1/d3eb200f-9650-4104-a59b-3f9b4fd75591.png" width='16' alt="TastyRewards" />
-    // },
-    // {
-    //     value: 'Gatorade',
-    //     text: 'Gatorade',
-    //     icon: <img src="https://i.imgur.com/9l1cft1.png" width='16' alt="Gatorade"
-    //      />
-    // }
+  {
+    value: "Cibc",
+    text: "Cibc",
+    icon: <img src="https://i.imgur.com/f8llVJY.png" width="16" alt="CIBC" />,
+  },
+  {
+    value: "MountainDew",
+    text: "MountainDew",
+    icon: (
+      <img src="https://i.imgur.com/ih7vLVS.png" width="16" alt="MountainDew" />
+    ),
+  },
+  {
+    value: "TastyRewards",
+    text: "TastyRewards",
+    icon: (
+      <img
+        src="https://image.em.pepsico.com/lib/fe4515707564067f771671/m/1/d3eb200f-9650-4104-a59b-3f9b4fd75591.png"
+        width="16"
+        alt="TastyRewards"
+      />
+    ),
+  },
+  {
+    value: "Gatorade",
+    text: "Gatorade",
+    icon: (
+      <img src="https://i.imgur.com/9l1cft1.png" width="16" alt="Gatorade" />
+    ),
+  },
+  {
+    value: "ShellEnergy",
+    text: "ShellEnergy",
+    icon: (
+      <img src="https://i.imgur.com/130DVZh.png" width="16" alt="ShellEnergy" />
+    ),
+  },
 ];
 
 export const listOfTypes = [
-    {
-        value: 'Email',
-        text: `Email`,
-        icon: <img src="https://i.imgur.com/tLZ85SF.png" width='16' alt="Email" />
-    },
-    {
-        value: 'Content Block',
-        text: 'Content Block',
-        icon: <img src="https://i.imgur.com/Wh3Y5ZC.png" width='16' alt="Content Block" />
-    },
-]
+  {
+    value: "Email",
+    text: `Email`,
+    icon: <img src="https://i.imgur.com/tLZ85SF.png" width="16" alt="Email" />,
+  },
+  {
+    value: "Content Block",
+    text: "Content Block",
+    icon: (
+      <img
+        src="https://i.imgur.com/Wh3Y5ZC.png"
+        width="16"
+        alt="Content Block"
+      />
+    ),
+  },
+];
 
 export const listOfView = [
-    {
-        value: 'Desktop',
-        text: `Desktop`,
-        icon: <img src="https://i.imgur.com/c4jDmGP.png" width='16' alt="Desktop" />
-    },
-    {
-        value: 'Mobile',
-        text: 'Mobile',
-        icon: <img src="https://i.imgur.com/RiXLAMx.png" width='16' alt="Mobile" />
-    },
-]
-
+  {
+    value: "Desktop",
+    text: `Desktop`,
+    icon: (
+      <img src="https://i.imgur.com/c4jDmGP.png" width="16" alt="Desktop" />
+    ),
+  },
+  {
+    value: "Mobile",
+    text: "Mobile",
+    icon: <img src="https://i.imgur.com/RiXLAMx.png" width="16" alt="Mobile" />,
+  },
+];
